117c74a5140bd7dd5806ec8e26597d3974152655
kidusm001 <kidusmesfin27@gmail.com>
2025-03-17 23:57:41 +0300

    adding leaf nodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_basic(self):
@@ -23,6 +23,23 @@ class TestHTMLNode(unittest.TestCase):
         node2.props["new-prop"] = "value"
         self.assertEqual(node1.props, {})  # Ensure node1.props wasn't affected by node2
         self.assertEqual(node2.props_to_html(), ' style="color: red;" new-prop="value"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode(tag="p", value="this is the content", props={"class": "text-bold", "id": "para1"})
+        self.assertEqual(node.to_html(), "<p class=\"text-bold\" id=\"para1\">this is the content</p>")
+
+    def test_leaf_to_html_empty(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode(tag="a", value="link", props={"href": "https://example.com?x=1&y=2", "title": 'He said "hello"'})
+        self.assertEqual(node.to_html(), '<a href="https://example.com?x=1&y=2" title="He said \"hello\"">link</a>')
+
 
 if __name__ == "__main__":
     unittest.main()
